380633bb97c19b553c613ae11e9df1667d727b56
Gabriel Bomfim <gabrielbomfimfelipe@gmail.com>
2023-02-11 17:03:04 -0300

    tabuleiro montado usando js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
-const BACK = "card-back";
+const BACK = "card_back";
+const CARD = "card";
+const ICON = 'icon'
 
 let techs = [
   "bootstrap",
@@ -18,36 +20,70 @@ startGame();
 function startGame() {
   let cards = createCardsFromTechs(techs);
   shuffleCards(cards);
-  console.log(cards)
+  console.log(cards);
+
+  initializeCards(cards);
+}
+
+function initializeCards(cards) {
+  let gamerBoard = document.getElementById("gamerBoard");
+  cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    cardElement.addEventListener("click", flipCard);
+    gamerBoard.appendChild(cardElement);
+    createCardContent(card, cardElement);
+  });
+}
+
+function createCardContent(card, cardElement) {
+  creatCardFace(FRONT,card,cardElement)
+  creatCardFace(BACK,card,cardElement)
+}
+
+function creatCardFace(face,card,element){
+  let cardElementFace = document.createElement('div');
+  cardElementFace.classList.add(face);
+  if(face === FRONT){
+      let iconElement = document.createElement('img');
+      iconElement.classList.add(ICON)
+      iconElement.src = "./images/" + card.icon + ".png"
+      cardElementFace.appendChild(iconElement);
+  }else{
+    cardElementFace.innerHTML = "&lt/&gt"
+  }
+  element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
-
   let currentIdex = cards.length;
   let randomIndex = 0;
 
   while (currentIdex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIdex)
+    randomIndex = Math.floor(Math.random() * currentIdex);
     currentIdex--;
 
-    [cards[randomIndex], cards[currentIdex]] = [cards[currentIdex], cards[randomIndex]]
+    [cards[randomIndex], cards[currentIdex]] = [
+      cards[currentIdex],
+      cards[randomIndex],
+    ];
   }
 }
 
 function createCardsFromTechs(techs) {
   let cards = [];
 
-  for (let tech of techs) {
+  techs.forEach((tech) => {
     cards.push(createPairFromTechs(tech));
-  }
+  });
   return cards.flatMap((pair) => pair);
 }
-
 function createPairFromTechs(tech) {
   return [
-    { id: createIdWithTechs(tech),
-      icon: tech,
-      flipped: false },
+    { id: createIdWithTechs(tech), icon: tech, flipped: false },
     {
       id: createIdWithTechs(tech),
       icon: tech,
@@ -59,3 +95,5 @@ function createPairFromTechs(tech) {
 function createIdWithTechs(tech) {
   return tech + parseInt(Math.random() * 1000);
 }
+
+function flipCard() {}
